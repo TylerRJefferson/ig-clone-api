435e6f0a4fd82dc02ea5e364c406445005a7b194
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-16 13:37:37 -0500

    basic setup

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
-const functions = require("firebase-functions");
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors());
+app.use(express.json()); //any time doing anything other than .get
+
+app.get("/photos", getAllPhotos)
+app.post("/photos", addNewPhoto)
+
+export const api = functions.https.onRequest(app);
