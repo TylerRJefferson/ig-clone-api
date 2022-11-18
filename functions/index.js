85c7eb6fd85d190d9696d4d46afca9c5a62668ab
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-17 21:05:29 -0500

    patch route for like button

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { getAllPhotos, addNewPhoto } from "./src/photos.js";
+import { getAllPhotos, addNewPhoto, addLike } from "./src/photos.js";
 
 const app = express();
 app.use(cors());
@@ -9,5 +9,6 @@ app.use(express.json()); //any time doing anything other than .get
 
 app.get("/photos", getAllPhotos)
 app.post("/photos", addNewPhoto)
+app.patch("/photos/:photoId", addLike)
 
 export const api = functions.https.onRequest(app);
